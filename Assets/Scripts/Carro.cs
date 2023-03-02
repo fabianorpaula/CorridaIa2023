7dd353e9d240d1886c2e84a8a3876175c42184be
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-03-02 17:16:24 -0300

    Nova Linha de Corrida Pela Terra

--- Assets/Scripts/Carro.cs
+++ Assets/Scripts/Carro.cs
@@ -9,14 +9,42 @@ public class Carro : MonoBehaviour
     private NavMeshAgent Car;
     //Destino para segue
     public GameObject Destiny;
+    public List<GameObject> Destinos;
+    public int ponteiro;
      void Start()
     {
         Car = GetComponent<NavMeshAgent>();
+        ponteiro = 0;
+        Destiny = Destinos[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Envia o Carro para direção certa
         Car.SetDestination(Destiny.transform.position);
+        //Distancia entre dois pontos
+        float distancia = Vector3.Distance(transform.position, Destiny.transform.position);
+        if (distancia < 5)
+        {
+
+            if(Destinos[ponteiro ].gameObject.tag == "Lento")
+            {
+                Car.speed = Random.Range(20, 30);
+            }
+            else if (Destinos[ponteiro].gameObject.tag == "Medio")
+            {
+                Car.speed = Random.Range(30, 40);
+            }
+            else if (Destinos[ponteiro].gameObject.tag == "Rapido")
+            {
+                Car.speed = Random.Range(40, 50);
+            }
+
+            //Aumenta ponteiro
+            ponteiro++;
+            if(ponteiro > Destinos.Count-1) { ponteiro = 0; }
+            Destiny = Destinos[ponteiro];
+        }
     }
 }
